4c8aa91b7e25a1ebe95d10602ff8bc728d78073b
saojiashutosh <ashutoshsaoji1@gmail.com>
2025-01-22 00:13:55 +0530

    updated CSS for form and navbar

--- app/forms.py
+++ app/forms.py
@@ -1,20 +1,39 @@
 from django import forms
 from django.forms import ModelForm
-from .models import user,blog
+from .models import user, blog
 
 class UserForm(ModelForm):
     class Meta:
         model = user
         fields = "__all__"
 
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = f'Enter {field.label}'  # Optional placeholder for input fields
+
 class SigninForm(forms.Form):
     username = forms.CharField(label="Username", max_length=10)
     password = forms.CharField(label="Password", max_length=16)
+
+    def __init__(self, *args, **kwargs):
+        super(SigninForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = f'Enter {field.label}'  # Optional placeholder for input fields
 
 class BlogForm(forms.ModelForm):
     class Meta:
         model = blog
         fields = ['title', 'content']
-        labels = {'title':'Blog Title',
-                  'content':'Enter Blog Content'}
-    
+        labels = {
+            'title': 'Blog Title',
+            'content': 'Enter Blog Content'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(BlogForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = f'Enter {field.label}'  # Optional placeholder for input fields
